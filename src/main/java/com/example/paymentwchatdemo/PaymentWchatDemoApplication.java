cd186edfc9cf59ef0b9c3d008e2da20f35ba24b5
aslin23 <2327920702@qq.com>
2024-02-29 22:22:30 +0800

    Initial commit

---
+++ src/main/java/com/example/paymentwchatdemo/PaymentWchatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentwchatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWchatDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentWchatDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paymentwchatdemo/PaymentWchatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentwchatdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWchatDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
